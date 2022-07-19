e397801d49569278d679450cd55fc025283d5019
John Swan <johneswan3@protonmail.com>
2022-07-18 19:41:10 -0600

    Work on mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import Link from "next/link";
-import { FaUserAlt } from "react-icons/fa";
+import { FaHome, FaUserAlt } from "react-icons/fa";
+import { BiMenuAltRight, BiX } from "react-icons/bi";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
-    <nav className="p-4 bg-slate-50 shadow-lg border-b border-slate-300 shadow-slate-300 h-[80px] flex text-center items-center">
+    <nav className="fixed w-full p-4 bg-slate-50 shadow-lg border-b border-slate-300 shadow-slate-300 h-[80px] flex text-center items-center">
       <div className="flex flex-row justify-between max-w-[1920px] mx-auto w-full text-center items-center">
         <div className="p-2">
           <div className="p-2">
@@ -17,7 +25,7 @@ export default function Navbar() {
             </Link>
           </div>
         </div>
-        <div>
+        <div className="hidden md:inline">
           <ul className="flex flex-row text-center items-center tracking-wide space-x-3 px-2 py-1">
             <li className="hover:text-fuchsia-600 transition-colors duration-150 ease-in">
               <Link href="/">
@@ -56,7 +64,31 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
+        <div className="block md:hidden">
+          <div
+            className=" space-x-4 p-1 shadow-md hover:shadow border-slate-300 border hover:text-fuchsia-600 transition-all duration-100 ease-in shadow-slate-400 rounded-full"
+            onClick={handleNav}
+          >
+            {nav ? <BiMenuAltRight size={25} /> : <BiX size={25} />}
+          </div>
+          <div
+            className={
+              nav
+                ? "md:hidden fixed right-[-100%] top-20 h-[90%] ease-out duration-200"
+                : "md:hidden fixed right-0 top-20 w-[100%] sm:w-[50%] h-[100%] bg-slate-50 ease-in duration-200 z-10 shadow-lg border-l border-slate-300 shadow-slate-400"
+            }
+          >
+            <ul className="flex flex-col w-full text-left p-2 space-y-4">
+              <li>home</li>
+              <li>custom</li>
+              <li>templates</li>
+              <li>contact</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
